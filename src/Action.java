0b13b01e9772a3372a214dfa11859f6774d49652
seri-code <75782257+seri-code@users.noreply.github.com>
2020-12-29 17:32:16 +0900

    Create Action.java

---
+++ src/Action.java
@@ -0,0 +1,21 @@
+package beans;
+
+public class Action {
+	private boolean isRedirect;
+	private String page;
+	
+	public boolean isRedirect() {
+		return isRedirect;
+	}
+	public void setRedirect(boolean isRedirect) {
+		this.isRedirect = isRedirect;
+	}
+	public String getPage() {
+		return page;
+	}
+	public void setPage(String page) {
+		this.page = page;
+	}
+	
+	
+}
